09356b22b9d00b865df6e1dee6bc811535b166c8
Lari Rauno <tuutti@iki.fi>
2018-04-21 02:29:36 +0300

    Removed unused travis before script

--- .travis-before-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e $DRUPAL_TI_DEBUG
-
-if [ "$DRUPAL_TI_CORE_BRANCH" == "8.5.x" ]; then
-  cd "$DRUPAL_TI_DRUPAL_BASE/drupal"
-
-  composer update phpunit/phpunit --with-dependencies --no-progress
-fi
